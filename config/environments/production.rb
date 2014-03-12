95441b177a7bfa064376d21b330c2bbbf89b621f
damouse <mickey.barboi@gmail.com>
2014-03-12 15:37:28 -0500

    precompiled assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,5 +84,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-    config.assets.precompile += ['application.css', 'landing.css']
+  config.assets.precompile += ['application.css', 'landing.css']
 end

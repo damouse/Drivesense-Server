5b7ff665365a663812d759acd9814ee4097716ca
Mickey Barboi <damouse007@gmail.com>
2014-06-12 21:13:11 -0500

    more fun with heroku;

--- db/migrate/20140613020635_change_time_to_timestamp.rb
+++
@@ -1,6 +0,0 @@
-class ChangeTimeToTimestamp < ActiveRecord::Migration
-  def change
-    change_column :patterns, :start_time, :timestamp
-    change_column :patterns, :end_time, :timestamp
-  end
-end

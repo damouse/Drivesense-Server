ec89e9f8294ed13d29b31946cb27888acfab1ad3
Nicholas Hyatt <nhyatt@wisc.edu>
2014-06-19 17:17:09 -0500

    fixed auth mistake in app_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,6 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_filter :authenticate_user_from_token!
-  # This is Devise's authentication
-  before_filter :authenticate_user!
 
  	def authenticate_user_from_token!
 	    user_email = params[:user_email].presence

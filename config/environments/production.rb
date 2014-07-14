5ccc6b0f59615cfba305578c017b2f0b8ef88a89
Mickey Barboi <damouse007@gmail.com>
2014-07-14 18:55:49 -0500

    added mailer information in config files

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,6 +86,8 @@ Rails.application.configure do
 
   config.assets.precompile += ['application.css', 'landing.css']
 
-  config.action_mailer.default_url_options = { :host => 'drivesense.heroku.com' }
+  #config.action_mailer.default_url_options = { :host => 'drivesense.heroku.com' }
+  config.action_mailer.delivery_method = :mailgun
+
   config.force_ssl = true
 end

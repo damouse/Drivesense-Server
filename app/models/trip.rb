72aebdd3722f85620c84ed888dd05f85ae3ddc5a
Nicholas Hyatt <nhyatt@wisc.edu>
2014-07-18 12:17:59 -0500

    custom markers

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -11,20 +11,31 @@ class Trip < ActiveRecord::Base
   def gmaps_endpoints
     coordinates = self.coordinates.sort_by &:time_stamp
     endpoints = [coordinates.first, coordinates.last]
+
     
+    count = 0
     hash = Gmaps4rails.build_markers(endpoints) do |coord, marker|
+      time = Time.at(coord.time_stamp/1000)
       marker.lat coord.latitude
       marker.lng coord.longitude
-      marker.infowindow "Information"
-      marker.picture "/assets/stop.png"
-
-      unless coord.time_stamp.nil?
-        time = Time.at(coord.time_stamp/1000)
-        marker.title time.strftime('%r')
+      if count == 0
+        marker.infowindow "Start of Trip (#{time.strftime('%r')})"
+        marker.picture({
+          :url => "/assets/car_map.png",
+          :width   => 32,
+          :height  => 35
+        })
       else
-        marker.title "End Point"
+        marker.infowindow "End of Trip (#{time.strftime('%r')})"
+        marker.picture({
+          :url => "/assets/stop.png",
+          :width   => 32,
+          :height  => 35
+        })
       end
 
+      marker.title time.strftime('%r')
+      count += 1
     end
 
     hash.to_json

28b4dcf55a3af5dce39b64b462981011d972697a
Nicholas Hyatt <nhyatt@wisc.edu>
2014-07-18 20:11:26 -0500

    working on mailgun

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,5 +89,18 @@ Rails.application.configure do
   #config.action_mailer.default_url_options = { :host => 'drivesense.heroku.com' }
   config.action_mailer.delivery_method = :mailgun
 
+
+ActionMailer::Base.smtp_settings = {
+  :port           => ENV['MAILGUN_SMTP_PORT'],
+  :address        => ENV['MAILGUN_SMTP_SERVER'],
+  :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+  :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+  :domain         => 'knowmydrive.com',
+  :authentication => :plain,
+}
+ActionMailer::Base.delivery_method = :smtp
+
+
+
   config.force_ssl = true
 end

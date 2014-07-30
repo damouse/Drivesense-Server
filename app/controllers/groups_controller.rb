e604107710a83d88407c2913d4d8ed8700d0b619
Nicholas Hyatt <nhyatt@wisc.edu>
2014-07-30 14:55:30 -0500

    css issues(forms,navbar,landing), About page update

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -6,6 +6,7 @@ class GroupsController < ApplicationController
   before_action :already_owns_group, only: [:new, :create]
   before_action :has_invitation, only: [:accept, :decline]
   before_action :remove_permission, only: [:remove]
+  before_action :group_params, only: [:create]
   
   def index
     @groups = Group.all

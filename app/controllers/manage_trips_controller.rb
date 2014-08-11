ed3283a83d391b41c597223bd4ae944fac448fb0
Mickey Barboi <damouse007@gmail.com>
2014-08-11 18:30:07 -0500

    quick fix to remove json rendering and return the manage trips page back to functionality

--- app/controllers/manage_trips_controller.rb
+++ app/controllers/manage_trips_controller.rb
@@ -8,24 +8,24 @@ class ManageTripsController < ApplicationController
   def manage_trips
     gon.current_user = current_user.id 
 
-    #is member admin of a group?
-    group = Group.find_by_id(current_user.admins_id)
+    # #is member admin of a group?
+    # group = Group.find_by_id(current_user.admins_id)
 
-    #is user a member of a group?
-    group = Group.find_by_id(current_user.group_id) if group.nil?
+    # #is user a member of a group?
+    # group = Group.find_by_id(current_user.group_id) if group.nil?
 
-    #user is not a member of any group, return users' trip information
-    if group.nil?
-      render json: {trips: current_user.trips.to_json} and return
-      @group_data = current_user.trips.to_json and return
-    end
+    # #user is not a member of any group, return users' trip information
+    # if group.nil?
+    #   render json: {trips: current_user.trips.to_json} and return
+    #   @group_data = current_user.trips.to_json and return
+    # end
 
-    render json: {groups: group.as_json(include: 
-          {children: 
-            {include: :trips}
-          }
-        )
-      }
+    # render json: {groups: group.as_json(include: 
+    #       {children: 
+    #         {include: :trips}
+    #       }
+    #     )
+    #   }
 
       #   render :json => {trips: trips.as_json(:include => 
       #     {:score => {:include => 
